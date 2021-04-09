e9d8dd30376c2935f0ff80839eeb5417fe48252c
Radoslaw Bura <radoslaw.bura@gmail.com>
2021-04-09 09:05:34 +0200

    add create a new post form, add search term form, add sorting and order, improve styles

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 // javascript for index.html
 
 const container = document.querySelector('.blogs');
+const searchForm = document.querySelector('form.search');
 
-const renderPosts = async () => {
-  let uri = 'http://localhost:3000/posts';
+const renderPosts = async (term) => {
+  let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+
+  if(term) {
+    uri += `&q=${term}`;
+  }
 
   const res = await fetch(uri);
   const posts = await res.json();
@@ -13,14 +18,21 @@ const renderPosts = async () => {
     template += `
       <div class="posts">
         <h2>${post.title}</h2>
-        <p><small>${post.likes}</small></p>
+        <p><small>Likes: ${post.likes}</small></p>
         <p>${post.body.slice(0, 200)}</p>
-        <a href="/details.html?id=${post.id}">read more..</a>
+        <div>
+          <a href="/details.html?id=${post.id}">read more..</a>
+        </div>
       </div>
     `;
   });
 
   container.innerHTML = template;
 }
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  renderPosts(searchForm.term.value.trim());
+});
 
 window.addEventListener('DOMContentLoaded', () => renderPosts());
